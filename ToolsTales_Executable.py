742eb443c6bc9ae54de97a02bc1c2c8e7dbf62b9
Stewie <fortiersteven1@gmail.com>
2022-03-27 15:22:50 -0400

    Add parameter to get_Releases function for last release

--- ToolsTales_Executable.py
+++ ToolsTales_Executable.py
@@ -9,11 +9,14 @@ import requests
 
 SCRIPT_VERSION = "0.3"
 
-def get_All_Releases(org, repo_name):
+def get_Releases(org, repo_name, latest=False):
     
     #git_url = "https://api.github.com/repos/SymphoniaLauren/Tales-of-Rebirth/releases"
     git_url = "https://api.github.com/repos/{}/{}/releases".format(org, repo_name)
     
+    if latest:
+        git_url = git_url+"/latest"
+        
     header = {
         "Accept":"application/vnd.github.v3+json" 
     }
@@ -22,7 +25,7 @@ def get_All_Releases(org, repo_name):
     json_res = json.loads(res.text)
     
     return json_res
-
+    
 def get_directory_path(path):
     return os.path.dirname(os.path.abspath(path))
 
