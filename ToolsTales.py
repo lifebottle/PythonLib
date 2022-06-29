499a70021658798e7f03056d2947c97db6551c62
Stewie <fortiersteven1@gmail.com>
2022-06-28 22:19:34 -0400

    Update parameters of generate_xdelta_patch

--- ToolsTales.py
+++ ToolsTales.py
@@ -48,11 +48,12 @@ class ToolsTales:
         with open("../{}/Data/{}/Menu/MenuFiles.json".format(repo_name, gameName)) as f:
            self.menu_files_json = json.load(f)
            
-    def generate_xdelta_patch(repo_name, xdelta_name):
-    
+    def generate_xdelta_patch(self, xdelta_name):
+        
+        
         print("Create xdelta patch")
-        original_path = "../Data/{}/Disc/Original/{}.iso".format(repo_name, repo_name)
-        new_path = "../Data/{}/Disc/New/{}.iso".format(repo_name, repo_name)
+        original_path = "../Data/{}/Disc/Original/{}.iso".format(self.repo_name, self.repo_name)
+        new_path = "../Data/{}/Disc/New/{}.iso".format(self.repo_name, self.repo_name)
         subprocess.run(["xdelta", "-f", "-s", original_path, new_path, xdelta_name])
            
     def mkdir(self, d):
