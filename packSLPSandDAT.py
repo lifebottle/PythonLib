39528ff1151de36dd5906b9baf9449876883dada
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2022-08-05 18:04:08 -0400

    Add script to insert SLPS + DAT.bin

--- packSLPSandDAT.py
+++ packSLPSandDAT.py
@@ -1,4 +1,14 @@
 import ToolsTOR
+import shutil
+
 tool = ToolsTOR.ToolsTOR("TBL_All.json")
+
+print("Packing files into SLPS_254.50")
 tool.pack_Menu_File("../Data/Tales-Of-Rebirth/Disc/Original/SLPS_254.50")
+
 tool.pack_Main_Archive()
+shutil.copy("../Data/Tales-Of-Rebirth/Menu/New/SLPS_254.50", "../Data/Tales-Of-Rebirth/Disc/New/SLPS_254.50")
+
+#Copy Original Iso
+print("Copying Rebirth Iso in New Folder")
+shutil.copy("C:/Users/Nick/Documents/Data/Tales-Of-Rebirth/Disc/Original/Tales of Rebirth.iso", "../Data/Tales-Of-Rebirth/Disc/New/Tales of Rebirth.iso")
